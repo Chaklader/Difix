ed26c88e3d1d3ca1ff56818bf03ddf1bcdd7ab61
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-06-26 13:43:34 +0600

    added support for jpeg

--- cheack_images_data.sh
+++ cheack_images_data.sh
@@ -2,7 +2,8 @@ python - <<'PY'
 from pathlib import Path
 from PIL import Image
 folder = Path('~/datasets/colmap_workspace/images').expanduser()
-for img in sorted(folder.glob('*.jpg'))[:5]:
+# include .jpg and .jpeg (case sensitive)
+for img in sorted(folder.glob('*.jp*g'))[:5]:
     w, h = Image.open(img).size
     print(img.name, f'{w}x{h}')
 PY
