1f1f5c42f0f66ec8923729980c3e20e0adbc0871
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-06-28 21:17:34 +0600

    added PLY to SPZ converter

---
+++ src/spz_py/compression.py
@@ -0,0 +1,19 @@
+import gzip
+
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+def decompress_gzipped(data: bytes) -> bytes:
+    try:
+        return gzip.decompress(data)
+    except Exception as error:
+        log.error("Error decompressing gzipped data:", error)
+        raise
+
+def compress_gzipped(data: bytes) -> bytes:
+    try:
+        return gzip.compress(data)
+    except Exception as error:
+        log.error("Error compressing gzipped data:", error)
+        raise 
