78f265a93b9debc3b2eba517bae3ff1d500901d6
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-06-29 08:44:58 +0600

    added

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Difix custom modules.
+
+Makes the `src` directory importable, allowing statements like
+`from src.pipeline_difix import DifixPipeline` to work when the project
+root is on `PYTHONPATH` (which is the default when you run Python from
+this directory).
+"""
