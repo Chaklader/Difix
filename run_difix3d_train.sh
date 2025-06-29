3741b190ec1bd89d8410ba3926498a88381c3766
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-06-29 15:17:46 +0600

    updated trainig command

--- run_difix3d_train.sh
+++ run_difix3d_train.sh
@@ -53,7 +53,6 @@ ns-train splatfacto \
   --pipeline.model.use_bilateral_grid True \
   --pipeline.model.densify-grad-thresh 0.0002 \
   --pipeline.model.refine-every 50 \
-  --pipeline.model.max-num-points -1 \
   --pipeline.model.spatial-lr-scale 0.1 \
   --pipeline.model.opacity-loss-weight 0.0005 \
   --optimizer.lr 0.002 \
