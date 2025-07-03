866039688667f6cd73b42193d0aff8a29f4d4483
Warren Pfefferu <warren@neuron3d.com>
2025-07-03 05:37:46 +0000

    updated the location for finetuned model

--- run_difix3d_gsplat_finetune.sh
+++ run_difix3d_gsplat_finetune.sh
@@ -1,8 +1,9 @@
 # Set up the Difix3D enhancement command
-SCENE_ID="difix3d_20250701_195855"
+# Set up the Difix3D enhancement command
+SCENE_ID="difix3d_20250703_025202"
 DATA_DIR="/home/azureuser/datasets/colmap_processed"
-CKPT_PATH="converted_checkpoints/gsplat_model_corrected.pt"
-OUTPUT_DIR="outputs/difix3d_enhanced/${SCENE_ID}"
+CKPT_PATH="finetune/gsplat_init.pt"
+OUTPUT_DIR="finetune/difix3d_enhanced/${SCENE_ID}"
 
 # Run Difix3D enhancement
 CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py default \
