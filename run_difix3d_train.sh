fead5c1141c744926549ceff4b0bb1e3b51f934b
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-07-14 15:59:00 +0600

    setup smoke test

--- run_difix3d_train.sh
+++ run_difix3d_train.sh
@@ -1,15 +1,17 @@
 # Set up the Difix3D enhancement command
-SCENE_ID="difix3d_20250701_195855"
+SCENE_ID="difix3d_2025_07_14"
 DATA_DIR="/home/azureuser/datasets/colmap_processed"
 CKPT_PATH="NeRF.pt"
 OUTPUT_DIR="/mnt/nvme0n1/azureuser/finetune/difix3d_enhanced/${SCENE_ID}"
 
 # Run Difix3D enhancement
-CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py mcmc \
+CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py default \
     --data_dir "${DATA_DIR}" \
     --data_factor 1 \
+    --batch_size 8 \
     --result_dir "${OUTPUT_DIR}" \
     --no-normalize-world-space \
-    --test_every 8 \
-    --max_steps 34100 \
+    --test_every 2 \
+    --max_steps 5000 \
+    --eval_steps "[2000, 4000, 5000]" \
     --ckpt "${CKPT_PATH}"
