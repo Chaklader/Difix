e358af4b8fa5dd7165057ac04a208085e9de7817
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-07-15 11:55:28 +0600

    added

--- run_difix3d_train.sh
+++ run_difix3d_train.sh
@@ -1,6 +1,7 @@
 SCENE_ID="difix3d_2025_07_14"
 DATA_DIR="/home/azureuser/datasets/colmap_processed"
-CKPT_PATH="NeRF.pt"
+# CKPT_PATH="NeRF.pt"
+CKPT_PATH="/mnt/nvme0n1/azureuser/finetune/difix3d_enhanced/difix3d_2025_07_14/ckpts/ckpt_32099_rank0.pt"
 OUTPUT_DIR="/mnt/nvme0n1/azureuser/finetune/difix3d_enhanced/${SCENE_ID}"
 
 CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py default \
@@ -14,7 +15,6 @@ CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py default 
     --eval_steps 32100 34100 35100 \
     --save_steps 32100 34100 35100 \
     --fix_steps 32000 34000 35000 \
-    --compression png \
     --ckpt "${CKPT_PATH}"
 
 echo "Difix3D training completed."
