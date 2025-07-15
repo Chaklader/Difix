ce0c29d5dee5ea7acea2a8b337cc5367b25e6978
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-07-15 14:07:33 +0600

    extend to 40k

--- run_difix3d_train.sh
+++ run_difix3d_train.sh
@@ -1,7 +1,7 @@
 SCENE_ID="difix3d_2025_07_14"
 DATA_DIR="/home/azureuser/datasets/colmap_processed"
 # CKPT_PATH="NeRF.pt"
-CKPT_PATH="/mnt/nvme0n1/azureuser/finetune/difix3d_enhanced/difix3d_2025_07_14/ckpts/ckpt_32099_rank0.pt"
+CKPT_PATH="/mnt/nvme0n1/azureuser/finetune/difix3d_enhanced/difix3d_2025_07_14/ckpts/ckpt_35499_rank0.pt"
 OUTPUT_DIR="/mnt/nvme0n1/azureuser/finetune/difix3d_enhanced/${SCENE_ID}"
 
 CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py default \
@@ -11,11 +11,11 @@ CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py default 
     --result_dir "${OUTPUT_DIR}" \
     --no-normalize-world-space \
     --test_every 10 \
-    --max_steps 35500 \
-    --eval_steps 32100 34100 35100 \
-    --save_steps 32100 34100 35100 \
-    --fix_steps 32000 34000 35000 \
+    --max_steps 40000 \
+    --eval_steps 32100 34100 35100 37100 39100 \
+    --save_steps 32100 34100 35100 37100 39100 \
+    --fix_steps 32000 34000 35000 37000 39000 \
     --ckpt "${CKPT_PATH}"
 
-echo "Difix3D training completed."
+echo "Difix3D training completed for 40.000 steps"
 
