0603e85d68d5f42399e696ac12c49439e3f01277
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-07-16 19:37:14 +0600

    added

--- tools/render_novel_views.sh
+++ tools/render_novel_views.sh
@@ -21,7 +21,7 @@ CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py default 
     --eval_steps 30001 \
     --save_steps 999999 \
     --fix_steps 30000 \
-    --render_traj_path spiral
+    --render_traj_path interp
 
 echo "Novel view rendering completed"
 echo "Rendered images saved to: ${OUTPUT_DIR}/renders/"
