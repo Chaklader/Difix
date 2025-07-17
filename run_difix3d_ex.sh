ed9f6c5cbf12a21fb34ccacedfc5e66e5f730f78
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-07-17 09:16:30 +0600

    added

--- run_difix3d_ex.sh
+++ run_difix3d_ex.sh
@@ -3,7 +3,7 @@ DATA_DIR="/home/azureuser/datasets/colmap_processed"
 CKPT_PATH="NeRF.pt"
 OUTPUT_DIR="/mnt/nvme0n1/azureuser/finetune/difix3d_enhanced/${SCENE_ID}"
 
-CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py mcmc \
+CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py default \
     --data_dir "${DATA_DIR}" \
     --data_factor 1 \
     --batch_size 8 \
@@ -16,8 +16,7 @@ CUDA_VISIBLE_DEVICES=0 python examples/gsplat/simple_trainer_difix3d.py mcmc \
     --fix_steps 31800 \
     --ckpt "${CKPT_PATH}" \
     --opacity_reg 0.01 \
-    --scale_reg 1e-4 \
-    --near_plane 0.05 \
+    --scale_reg 0.001 \
     --random_bkgd
 
 echo "Difix3D training completed for 32.000 steps"
