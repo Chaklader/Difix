0424886be0cf3f21ef9abc9b3fb658537be46e57
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2025-07-24 09:56:05 +0600

    added good model run

--- run_nerfacto.sh
+++ run_nerfacto.sh
@@ -10,8 +10,8 @@ set -euo pipefail
 export PYTHONPATH="${PYTHONPATH:-}:$(pwd)/src"
 
 # -----------------------------------------------------------------------------
-DATA_DIR="/mnt/nvme0n1/azureuser/datasets/nerfacto/nerf_data/colmap_process_bad"   # processed dataset
-RUN_NAME="nerfacto_bad_model_$(date +%Y%m%d_%H%M%S)"
+DATA_DIR="/mnt/nvme0n1/azureuser/datasets/nerfacto/nerf_data/colmap_process_good"   # processed dataset
+RUN_NAME="nerfacto_good_model_$(date +%Y%m%d_%H%M%S)"
 
 # -----------------------------------------------------------------------------
 # Launch Nerfacto training
